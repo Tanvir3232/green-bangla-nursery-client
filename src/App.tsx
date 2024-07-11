b145491324619405b69974340be66b6c5bebc76a
Tanvir3232 <tanvirahmadstudent@gmail.com>
2024-07-11 07:56:31 +0600

    set up project and install shadcn ui, tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="font-bold">Welcome to Green Bangla Nursery </h1>
+      <Button>Hellow World</Button>
+    </>
+  )
+}
+
+export default App
